2b568d8c69c6cbbe6df0f111075b096222dae7dd
Daniel Vaughan <dan@danielvaughan.com>
2018-07-08 19:20:38 +0100

    Renamed metadata content to info

--- api/model/model.go
+++ api/model/model.go
@@ -3,16 +3,16 @@ package model
 import "time"
 
 type Dataset struct {
-	Owner     string    `dynamodbav:"owner",json:"owner"`
-	DatasetId string    `dynamodbav:"dataset_id",json:"dataset_id"`
-	Created   time.Time `dynamodbav:"created" type:"timestamp" timestampFormat:"unix",json:"created"`
+	Owner     string    `dynamodbav:"owner" json:"owner"`
+	DatasetId string    `dynamodbav:"dataset_id" json:"dataset_id"`
+	Created   time.Time `dynamodbav:"created" type:"timestamp" timestampFormat:"unix" json:"created"`
 }
 
 type Metadata struct {
-	DatasetId   string    `dynamodbav:"dataset_id",json:"dataset_id"`
-	MetadataId  string    `dynamodbav:"metadata_id",json:"metadata_id"`
-	DescribedBy string    `dynamodbav:"described_by",json:"described_by"`
-	Created     time.Time `dynamodbav:"created" type:"timestamp" timestampFormat:"unix",json:"created"`
+	DatasetId   string    `dynamodbav:"dataset_id" json:"dataset_id"`
+	MetadataId  string    `dynamodbav:"metadata_id" json:"metadata_id"`
+	DescribedBy string    `dynamodbav:"described_by" json:"described_by"`
+	Created     time.Time `dynamodbav:"created" type:"timestamp" timestampFormat:"unix" json:"created"`
 }
 
 type ErrorMessage struct {
