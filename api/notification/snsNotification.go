ce4ef44ef6e16b7818506aa36ca369f016b89ebd
Daniel Vaughan <dan@danielvaughan.com>
2018-07-31 18:30:14 +0100

    removed duplicated vendor directory causing https://github.com/aws/aws-sdk-go/issues/2082

--- api/notification/snsNotification.go
+++ api/notification/snsNotification.go
@@ -1,6 +1,27 @@
 package notification
 
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+	"os"
+)
+
 type SNSNotification struct{}
+
+var snsSvc *sns.SNS
+
+func init() {
+	region := os.Getenv("AWS_REGION")
+	if ses, err := session.NewSession(&aws.Config{
+		Region: &region,
+	}); err != nil {
+		fmt.Println(fmt.Sprintf("Failed to connect to AWS: %s", err.Error()))
+	} else {
+		snsSvc = sns.New(ses)
+	}
+}
 
 func (SNSNotification) NotifyMetadataCreated(metadataUrl string) (err error) {
 	return nil
