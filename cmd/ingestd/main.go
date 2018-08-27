aaf5953cf02edbb5d3cccf9173dedb87107d3253
Daniel Vaughan <dan@danielvaughan.com>
2018-08-27 10:45:00 +0100

    Secured endpoints with auth0

--- cmd/ingestd/main.go
+++ cmd/ingestd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/auth0/go-jwt-middleware"
 	"github.com/codetaming/indy-ingest/cmd/ingestd/handlers"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
+	"net/http"
 	"os"
 )
 
@@ -23,18 +26,37 @@ func main() {
 
 	mw := jwtmiddleware.New(jwtmiddleware.Options{
 		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-			return []byte("secret"), nil
+			// Verify 'aud' claim
+			aud := "http://localhost:9000"
+			checkAud := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+			if !checkAud {
+				return token, errors.New("Invalid audience.")
+			}
+			// Verify 'iss' claim
+			iss := os.Getenv("AUTH0_ISSUER")
+			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+			if !checkIss {
+				return token, errors.New("Invalid issuer.")
+			}
+
+			cert, err := getPemCert(token)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+			return result, nil
 		},
 		SigningMethod: jwt.SigningMethodRS256,
 	})
 
 	r.HandleFunc("/validate", handlers.Validate).Methods("POST")
-	r.HandleFunc("/dataset", handlers.CreateDataset).Methods("POST")
-	r.HandleFunc("/dataset", handlers.ListDatasets).Methods("GET")
-	r.HandleFunc("/dataset/{datasetId}", handlers.GetDataset).Methods("GET")
-	r.HandleFunc("/dataset/{datasetId}/metadata", handlers.AddMetadata).Methods("POST")
-	r.HandleFunc("/dataset/{datasetId}/metadata", handlers.ListMetadata).Methods("GET")
-	r.HandleFunc("/dataset/{datasetId}/metadata/{metadataId}", handlers.GetMetadata).Methods("GET")
+	ar.HandleFunc("/dataset", handlers.CreateDataset).Methods("POST")
+	ar.HandleFunc("/dataset", handlers.ListDatasets).Methods("GET")
+	ar.HandleFunc("/dataset/{datasetId}", handlers.GetDataset).Methods("GET")
+	ar.HandleFunc("/dataset/{datasetId}/metadata", handlers.AddMetadata).Methods("POST")
+	ar.HandleFunc("/dataset/{datasetId}/metadata", handlers.ListMetadata).Methods("GET")
+	ar.HandleFunc("/dataset/{datasetId}/metadata/{metadataId}", handlers.GetMetadata).Methods("GET")
 
 	an := negroni.New(negroni.HandlerFunc(mw.HandlerWithNext), negroni.Wrap(ar))
 	r.PathPrefix("/").Handler(an)
@@ -42,4 +64,47 @@ func main() {
 	n := negroni.Classic()
 	n.UseHandler(r)
 	n.Run(":9000")
+}
+
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
+
+func getPemCert(token *jwt.Token) (string, error) {
+	cert := ""
+	resp, err := http.Get(os.Getenv("AUTH0_ISSUER") + ".well-known/jwks.json")
+
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	for k := range jwks.Keys {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		err := errors.New("Unable to find appropriate key.")
+		return cert, err
+	}
+
+	return cert, nil
 }
