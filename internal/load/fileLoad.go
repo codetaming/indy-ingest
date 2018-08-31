d7ef9e8441748df277cef12e678d949c489b249c
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-08-31 10:07:36 +0100

    fixed issue where input file was being closed early

--- internal/load/fileLoad.go
+++ internal/load/fileLoad.go
@@ -3,6 +3,7 @@ package load
 import (
 	"bytes"
 	"compress/gzip"
+	"fmt"
 	"io/ioutil"
 	"log"
 )
@@ -26,16 +27,27 @@ func (f *FileLoader) Start() {
 }
 
 func (f *FileLoader) Store(jsonData []byte, total *int, limit int) {
-	w.Write(jsonData)
+	_, err := w.Write(jsonData)
+	if err != nil {
+		log.Fatal(err)
+	}
 	*total++
 	if limit == 0 || *total < limit {
 		w.Write([]byte(",\n"))
+	} else {
+		fmt.Printf("Limit %d reached\n", limit)
 	}
 }
 
 func (f *FileLoader) Finish() {
-	w.Write([]byte("]"))
-	w.Close()
+	_, writeErr := w.Write([]byte("]"))
+	if writeErr != nil {
+		log.Fatal(writeErr)
+	}
+	closeError := w.Close()
+	if closeError != nil {
+		log.Fatal(closeError)
+	}
 	err := ioutil.WriteFile(f.outputFile, b.Bytes(), 0666)
 	if err != nil {
 		log.Fatal(err)
